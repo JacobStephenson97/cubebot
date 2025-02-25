a7167bf21b8828f3950c45d5084cb5b935f83c8d
Jacob Stephenson <jacobstephenson97@gmail.com>
2025-02-25 00:32:45 -0500

    added .env for token

--- main.go
+++ main.go
@@ -1,8 +1,20 @@
 package main
 
-import bot "cubebot/bot"
+import (
+	bot "cubebot/bot"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+)
 
 func main() {
-	bot.BotToken = ""
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	bot.BotToken = os.Getenv("DISCORD_TOKEN")
+
 	bot.Run() // call the run function of bot/bot.go
 }
