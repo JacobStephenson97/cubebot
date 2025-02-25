a571edcfc4e307cf582e037660614ff161d08e07
Jacob Stephenson <jacobstephenson97@gmail.com>
2025-02-25 00:26:35 -0500

    first init

---
+++ bot/bot.go
@@ -0,0 +1,122 @@
+package bot
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"os"
+	"os/signal"
+	"strings"
+
+	"github.com/bwmarrin/discordgo"
+)
+
+var BotToken string
+
+var (
+	RemoveCommands = flag.Bool("rmcmd", true, "Remove all commands after shutdowning or not")
+)
+
+func checkNilErr(e error) {
+	if e != nil {
+		log.Fatal("Error message")
+	}
+}
+
+func Run() {
+	// create a session
+	discord, err := discordgo.New("Bot " + BotToken)
+	checkNilErr(err)
+
+	// open session first
+	err = discord.Open()
+	checkNilErr(err)
+	defer discord.Close() // close session, after function termination
+
+	//slash commands
+	var commands = []*discordgo.ApplicationCommand{
+		{
+			Name: "start-draft",
+			// All commands and options must have a description
+			// Commands/options without description will fail the registration
+			// of the command.
+			Description: "Start a new draft",
+		},
+	}
+	var commandHandlers = map[string]func(s *discordgo.Session, i *discordgo.InteractionCreate){
+		"start-draft": func(s *discordgo.Session, i *discordgo.InteractionCreate) {
+			s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
+				Type: discordgo.InteractionResponseChannelMessageWithSource,
+				Data: &discordgo.InteractionResponseData{
+					Content: "Draft started",
+				},
+			})
+		},
+	}
+
+	discord.AddHandler(func(s *discordgo.Session, i *discordgo.InteractionCreate) {
+		if handler, ok := commandHandlers[i.ApplicationCommandData().Name]; ok {
+			handler(s, i)
+		}
+	})
+
+	// Register commands after opening the session
+	registeredCommands := make([]*discordgo.ApplicationCommand, len(commands))
+	for i, v := range commands {
+		cmd, err := discord.ApplicationCommandCreate(discord.State.User.ID, "885029273180700763", v)
+		if err != nil {
+			log.Panicf("Cannot create '%v' command: %v", v.Name, err)
+		}
+		registeredCommands[i] = cmd
+	}
+
+	// add a event handler
+	discord.AddHandler(newMessage)
+
+	// keep bot running until there is NO os interruption (ctrl + C)
+	fmt.Println("Bot running....")
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt)
+	<-c
+
+	if *RemoveCommands {
+		log.Println("Removing commands...")
+		// // We need to fetch the commands, since deleting requires the command ID.
+		// // We are doing this from the returned commands on line 375, because using
+		// // this will delete all the commands, which might not be desirable, so we
+		// // are deleting only the commands that we added.
+		// registeredCommands, err := s.ApplicationCommands(s.State.User.ID, *GuildID)
+		// if err != nil {
+		// 	log.Fatalf("Could not fetch registered commands: %v", err)
+		// }
+
+		for _, v := range registeredCommands {
+			err := discord.ApplicationCommandDelete(discord.State.User.ID, "885029273180700763", v.ID)
+			if err != nil {
+				log.Panicf("Cannot delete '%v' command: %v", v.Name, err)
+			}
+		}
+	}
+
+}
+
+func newMessage(discord *discordgo.Session, message *discordgo.MessageCreate) {
+
+	/* prevent bot responding to its own message
+	this is achived by looking into the message author id
+	if message.author.id is same as bot.author.id then just return
+	*/
+	if message.Author.ID == discord.State.User.ID {
+		return
+	}
+
+	// respond to user message if it contains `!help` or `!bye`
+	switch {
+	case strings.Contains(message.Content, "!help"):
+		discord.ChannelMessageSend(message.ChannelID, "Hello World😃")
+	case strings.Contains(message.Content, "!bye"):
+		discord.ChannelMessageSend(message.ChannelID, "Good Bye👋")
+		// add more cases if required
+	}
+
+}
---
+++ main.go
@@ -0,0 +1,8 @@
+package main
+
+import bot "cubebot/bot"
+
+func main() {
+	bot.BotToken = ""
+	bot.Run() // call the run function of bot/bot.go
+}
